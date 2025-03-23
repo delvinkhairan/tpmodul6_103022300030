2f036946296c75349714b298c60f13ea3ac940bf
delvinkhairan <delvinkhairan9558@gmail.com>
2025-03-23 17:22:54 +0700

    menambahkan design by contract

--- SayaTubeVideo.cs
+++ SayaTubeVideo.cs
@@ -1,34 +1,30 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Diagnostics;
 
-namespace tpmodul6_103022300030
+public class SayaTubeVideo
 {
+    private int id;
+    private string title;
+    private int playCount;
 
-    public class SayaTubeVideo
+    public SayaTubeVideo(string title)
     {
-        private int id;
-        private string title;
-        private int playCount;
-        public SayaTubeVideo(string title)
-        {
-            Random random = new Random();
-            this.id = random.Next(10000, 99999);
-            this.title = title;
-            this.playCount = 0; 
-        }
-
-        public void IncreasePlayCount(int count)
-        {
-            this.playCount += count;
-        }
-
-        public void PrintVideoDetails()
-        {
-            Console.WriteLine($"ID: {this.id}, Title: {this.title}, Play Count: {this.playCount}");
-        }
+        Debug.Assert(title.Length <= 100, "Title length must be less than or equal to 100 characters.");
+        this.title = title;
+        Random random = new Random();
+        this.id = random.Next(10000, 99999); 
+        this.playCount = 0; 
     }
 
+    public void IncreasePlayCount(int count)
+    {
+        Debug.Assert(count >= 0, "Play count must be non-negative.");
+        Debug.Assert(this.playCount + count <= 10000000, "Play count cannot exceed 10,000,000.");
+        this.playCount += count;
+    }
+
+    public void PrintVideoDetails()
+    {
+        Console.WriteLine($"ID: {this.id}, Title: {this.title}, Play Count: {this.playCount}");
+    }
 }
