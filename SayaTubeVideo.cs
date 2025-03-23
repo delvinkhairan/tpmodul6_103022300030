46b8086bfd7c7f0c1a11a912851f1424025a2362
delvinkhairan <delvinkhairan9558@gmail.com>
2025-03-23 17:00:10 +0700

    menambahkan class SayaTubeVideo

---
+++ SayaTubeVideo.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tpmodul6_103022300030
+{
+
+    public class SayaTubeVideo
+    {
+        private int id;
+        private string title;
+        private int playCount;
+        public SayaTubeVideo(string title)
+        {
+            Random random = new Random();
+            this.id = random.Next(10000, 99999);
+            this.title = title;
+            this.playCount = 0; 
+        }
+
+        public void IncreasePlayCount(int count)
+        {
+            this.playCount += count;
+        }
+
+        public void PrintVideoDetails()
+        {
+            Console.WriteLine($"ID: {this.id}, Title: {this.title}, Play Count: {this.playCount}");
+        }
+    }
+
+}
